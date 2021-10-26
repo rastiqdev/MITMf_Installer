797f21dd375f45f6d2de5314b694d5fbe0f61dfd
Louis <62338646+AQRRastiq@users.noreply.github.com>
2021-10-26 11:07:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+sudo export PATH="/home/user/.local/bin:$PATH"
+
 sudo apt-get install python2-dev python-setuptools libpcap0.8-dev libnetfilter-queue-dev libssl-dev libjpeg-dev libxml2-dev libxslt1-dev libcapstone-dev libffi-dev file
 
 wget https://github.com/AQRRastiq/MITMf_Installer/raw/main/libcapstone3_4.0.1%2Breally%2B3.0.5-1_amd64.deb -O libcapstone3_4.0.1+really+3.0.5-1_amd64.deb
@@ -7,8 +9,6 @@ sudo dpkg -i libcapstone3_4.0.1+really+3.0.5-1_amd64.deb
 wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
 
 python get-pip.py
-
-export PATH="/home/user/.local/bin:$PATH"
 
 pip install virtualenvwrapper
 
